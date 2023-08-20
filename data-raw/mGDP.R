e254145a02bb24b54bffa766bbe38f72fe543498
lyuben-ivanov <l.ivanov@feb.uni-sofia.bg>
2023-08-20 14:34:05 +0300

    add internal dataset maddison GDP and script to import it

---
+++ data-raw/mGDP.R
@@ -0,0 +1,11 @@
+## code to prepare `mGDP` dataset goes here
+
+mGDP <- readr::read_csv(
+  file = paste0(getwd(),"/data-raw/gdp-per-capita-maddison.csv"),
+  col_select = c(Entity:`GDP per capita`, -Code)) |>
+  tidyr::pivot_wider(names_from = Entity, values_from = `GDP per capita`) |>
+  dplyr::arrange(Year)
+
+usethis::use_data(mGDP, overwrite = TRUE, internal = TRUE)
+
+
