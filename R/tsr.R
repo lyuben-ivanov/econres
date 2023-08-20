1225aa69dccb4f6e5cb8f1f029cb249e5f070d21
lyuben-ivanov <l.ivanov@feb.uni-sofia.bg>
2023-08-20 15:01:12 +0300

    add function to plot raw (unadjasted) time series as lines

---
+++ R/tsr.R
@@ -0,0 +1,34 @@
+
+tsr <- function (
+    x,
+    y,
+    xlab = NULL,
+    main = NULL,
+    ...
+) {
+  graphics::matplot(
+    x = x,
+    y = y,
+    type = 'l',
+    bty = 'l',
+    col = 1,
+    xlab = xlab,
+    ylab = '',
+    xaxs = 'i',
+    axes = F,
+    main = main
+  )
+
+  graphics::axis(
+    side = 1,
+    line = 1
+  )
+
+  graphics::axis(
+    side = 2,
+    at = graphics::axTicks(side = 2),
+    labels = graphics::axTicks(side = 2),
+    las = 2
+  )
+
+}
