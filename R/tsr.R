43b8dc886e78a1babfea9a83c2528fc5bd476221
lyuben-ivanov <l.ivanov@feb.uni-sofia.bg>
2023-08-23 10:56:58 +0300

    indicate dependencies in `tsr()`

--- R/tsr.R
+++ R/tsr.R
@@ -62,10 +62,10 @@ tsr <- function (
     cex = 0.9
   )
 
-  text(
-    x = x |> dplyr::select(1) |> tail(n = 1),
-    y = x |> tail(n = 1) |> as.numeric() |> tail(n = -1L),
-    labels = x |> names() |> tail(n = -1L),
+  graphics::text(
+    x = x |> dplyr::select(1) |> utils::tail(n = 1),
+    y = x |> utils::tail(n = 1) |> as.numeric() |> utils::tail(n = -1L),
+    labels = x |> names() |> utils::tail(n = -1L),
     xpd = TRUE,
     pos = 4,
     cex = 0.9
